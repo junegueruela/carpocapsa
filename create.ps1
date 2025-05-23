c890185fbeb88a0c27d0b4aef5f47c52f770cd86
junegueruela <junegueruela@gmail.com>
2025-05-23 18:00:59 +0200

    Corrección del dump

--- create.ps1
+++ create.ps1
@@ -3,4 +3,6 @@ docker volume create backup
 docker network create miRed
 docker compose up -d
 ## Para Windows
+# Para que le de tiempo a arrancar al mySQL
+Start-Sleep -Seconds 30
 Get-Content .\dump.sql | docker exec -i mySqlDocker mysql -uroot -pcarpocapsa
--- create.sh
+++ create.sh
@@ -3,4 +3,6 @@
     docker network create miRed
     docker compose up -d
     ## Para Linux
+    # Para que le de tiempo a arrancar al mySQL
+    sleep 30
     docker exec -i mySqlDocker mysql -uroot -pcarpocapsa < dump.sql
