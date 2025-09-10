b6dc113064c61c2bfdb05168fdc3b64ab34236bf
junegueruela <junegueruela@gmail.com>
2025-09-10 13:20:16 +0200

    Todo cambiado para que funcione

--- cronML/hacerpredicciones.py
+++ cronML/hacerpredicciones.py
@@ -1,6 +1,10 @@
+import os
+
+# Forzar método seguro en Python 3.12 / Docker
+os.environ["JOBLIB_START_METHOD"] = "spawn"
+
 import predicciones as pr
 import conexionSGBD as cS
 import util as ut
-import os
 #os.chdir('D:\\VS Proyectos\\Control carpocapsa')
 pr.realizarPredicciones()
