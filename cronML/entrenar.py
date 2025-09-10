7c894358175a1884f72415912903c3b2274f25c9
junegueruela <junegueruela@gmail.com>
2025-09-10 12:36:55 +0200

    Modificación de n_jobs para que pueda entrenar en dockers.

--- cronML/entrenar.py
+++ cronML/entrenar.py
@@ -2,5 +2,9 @@ import predicciones as pr
 import conexionSGBD as cS
 import util as ut
 import os
+
+from joblib import Parallel, delayed
+os.environ["JOBLIB_NJOBS"] = "1"
+
 #os.chdir('D:\\VS Proyectos\\Control carpocapsa')
 pr.entrenarModelo()
